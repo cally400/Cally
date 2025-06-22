d8ab8c75176053e74d722da6d964152634089224
cally400 <battle.of.zajil@gmail.com>
2025-06-22 18:07:46 +0300

    Create love.blade.php

---
+++ resources/views/love.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ar">
+<head>
+    <meta charset="UTF-8">
+    <title>قلب أسود</title>
+    <style>
+        body {
+            background-color: white;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+        }
+        .heart {
+            font-size: 100px;
+            color: black;
+        }
+    </style>
+</head>
+<body>
+    <div class="heart">🖤</div>
+</body>
+</html>
